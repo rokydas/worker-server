a82e566921032122db413b446236803a8b764409
rokydas <droky136@gmail.com>
2021-11-12 13:35:03 +0600

    port spelling changed

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ app.use(user);
 app.use(service);
 app.use(order)
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
 	res.send("Hello world");
